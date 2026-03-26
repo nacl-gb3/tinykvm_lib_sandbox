b3e15351dff24af36d38f45dda366b61d0280852
nacl-gb3 <67475811+nacl-gb3@users.noreply.github.com>
2026-03-26 13:48:53 -0500

    changed code a bit for my own testing purposes

--- src/assert.hpp
+++ src/assert.hpp
@@ -1,12 +1,13 @@
 #include <cstdio>
+#include <cstdlib>
 
-void do_kassert(bool pred, const char* text, int line, const char* file, const char* function)
-{
-	if (pred) return;
-	fprintf(stderr, "%s:%d assertion failed in function %s: %s\n",
-		file, line, function, text);
-	std::abort();
+void do_kassert(bool pred, const char *text, int line, const char *file,
+                const char *function) {
+  if (pred)
+    return;
+  fprintf(stderr, "%s:%d assertion failed in function %s: %s\n", file, line,
+          function, text);
+  std::abort();
 }
 
-#define KASSERT(pred) \
-	do_kassert(pred, #pred, __LINE__, __FILE__, __FUNCTION__)
+#define KASSERT(pred) do_kassert(pred, #pred, __LINE__, __FILE__, __FUNCTION__)
--- src/simple.cpp
+++ src/simple.cpp
@@ -1,224 +1,252 @@
-#include <tinykvm/machine.hpp>
-#include <algorithm>
-#include <cstring>
-#include <cstdio>
 #include "assert.hpp"
 #include "load_file.hpp"
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <tinykvm/machine.hpp>
 
 #include <tinykvm/rsp_client.hpp>
-#define GUEST_MEMORY   0x80000000  /* 2GB memory */
-#define GUEST_WORK_MEM 1024UL * 1024*1024 /* MB working mem */
+#define GUEST_MEMORY 0x80000000             /* 2GB memory */
+#define GUEST_WORK_MEM 1024UL * 1024 * 1024 /* MB working mem */
 
-static uint64_t verify_exists(tinykvm::Machine& vm, const char* name)
-{
-	uint64_t addr = vm.address_of(name);
-	if (addr == 0x0) {
-//		fprintf(stderr, "Error: '%s' is missing\n", name);
-//		exit(1);
-	}
-	return addr;
+static uint64_t verify_exists(tinykvm::Machine &vm, const char *name) {
+  uint64_t addr = vm.address_of(name);
+  if (addr == 0x0) {
+    //		fprintf(stderr, "Error: '%s' is missing\n", name);
+    //		exit(1);
+  }
+  return addr;
 }
 
 inline timespec time_now();
 inline long nanodiff(timespec start_time, timespec end_time);
 
-int main(int argc, char** argv)
-{
-	if (argc < 2) {
-		fprintf(stderr, "Missing argument: 64-bit ELF binary\n");
-		exit(1);
-	}
-	std::vector<uint8_t> binary;
-	std::vector<std::string> args;
-	std::string filename = argv[1];
-	binary = load_file(filename);
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Missing argument: 64-bit ELF binary\n");
+    exit(1);
+  }
+  std::vector<uint8_t> binary;
+  std::vector<std::string> args;
+  std::string filename = argv[1];
+  binary = load_file(filename);
 
-	const tinykvm::DynamicElf dyn_elf = tinykvm::is_dynamic_elf(
-		std::string_view{(const char*)binary.data(), binary.size()});
-	if (dyn_elf.is_dynamic) {
-		// Add ld-linux.so.2 as first argument
-		static const std::string ld_linux_so = "/lib64/ld-linux-x86-64.so.2";
-		binary = load_file(ld_linux_so);
-		args.push_back(ld_linux_so);
-	}
+  const tinykvm::DynamicElf dyn_elf = tinykvm::is_dynamic_elf(
+      std::string_view{(const char *)binary.data(), binary.size()});
+  if (dyn_elf.is_dynamic) {
+    // Add ld-linux.so.2 as first argument
+    static const std::string ld_linux_so = "/lib64/ld-linux-x86-64.so.2";
+    binary = load_file(ld_linux_so);
+    args.push_back(ld_linux_so);
+  }
 
-	for (int i = 1; i < argc; i++)
-	{
-		args.push_back(argv[i]);
-	}
+  for (int i = 1; i < argc; i++) {
+    args.push_back(argv[i]);
+  }
 
-	tinykvm::Machine::init();
+  tinykvm::Machine::init();
 
-	tinykvm::Machine::install_unhandled_syscall_handler(
-	[] (tinykvm::vCPU& cpu, unsigned scall) {
-		switch (scall) {
-			case 0x10000:
-				cpu.stop();
-				break;
-			case 0x10001:
-				throw "Unimplemented";
-			case 0x10707:
-				throw "Unimplemented";
-			default:
-				printf("Unhandled system call: %u\n", scall);
-				auto regs = cpu.registers();
-				regs.rax = -ENOSYS;
-				cpu.set_registers(regs);
-		}
-	});
+  tinykvm::Machine::install_unhandled_syscall_handler(
+      [](tinykvm::vCPU &cpu, unsigned scall) {
+        switch (scall) {
+        case 0x10000:
+          cpu.stop();
+          break;
+        case 0x10001:
+          throw "Unimplemented";
+        case 0x10707:
+          throw "Unimplemented";
+        default:
+          printf("Unhandled system call: %u\n", scall);
+          auto regs = cpu.registers();
+          regs.rax = -ENOSYS;
+          cpu.set_registers(regs);
+        }
+      });
 
-	const std::vector<tinykvm::VirtualRemapping> remappings {
-		{
-			.phys = 0x0,
-			.virt = 0xC000000000,
-			.size = 512ULL << 20,
-		}
-	};
+  const std::vector<tinykvm::VirtualRemapping> remappings{{
+      .phys = 0x0,
+      .virt = 0xC000000000,
+      .size = 512ULL << 20,
+  }};
 
-	/* Setup */
-	const tinykvm::MachineOptions options {
-		.max_mem = GUEST_MEMORY,
-		.max_cow_mem = GUEST_WORK_MEM,
-		.reset_free_work_mem = 0,
-		.vmem_base_address = uint64_t(getenv("UPPER") != nullptr ? 0x40000000 : 0x0),
-		.remappings {remappings},
-		.verbose_loader = true,
-		.hugepages = (getenv("HUGE") != nullptr),
-		.relocate_fixed_mmap = (getenv("GO") == nullptr),
-		.executable_heap = dyn_elf.is_dynamic,
-	};
-	tinykvm::Machine master_vm {binary, options};
-	//master_vm.print_pagetables();
-	if (dyn_elf.is_dynamic) {
-		static const std::vector<std::string> allowed_readable_paths({
-			argv[1],
-			// Add all common standard libraries to the list of allowed readable paths
-			"/lib64/ld-linux-x86-64.so.2",
-			"/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2",
-			"/lib/x86_64-linux-gnu/libgcc_s.so.1",
-			"/lib/x86_64-linux-gnu/libc.so.6",
-			"/lib/x86_64-linux-gnu/libm.so.6",
-			"/lib/x86_64-linux-gnu/libpthread.so.0",
-			"/lib/x86_64-linux-gnu/libdl.so.2",
-			"/lib/x86_64-linux-gnu/libstdc++.so.6",
-			"/lib/x86_64-linux-gnu/librt.so.1",
-			"/lib/x86_64-linux-gnu/libz.so.1",
-			"/lib/x86_64-linux-gnu/libexpat.so.1",
-			"/lib/x86_64-linux-gnu/glibc-hwcaps/x86-64-v2/libstdc++.so.6",
-			"/lib/x86_64-linux-gnu/glibc-hwcaps/x86-64-v3/libstdc++.so.6",
-			"/lib/x86_64-linux-gnu/glibc-hwcaps/x86-64-v4/libstdc++.so.6",
-		});
-		master_vm.fds().set_open_readable_callback(
-			[&] (std::string& path) -> bool {
-				return std::find(allowed_readable_paths.begin(),
-					allowed_readable_paths.end(), path) != allowed_readable_paths.end();
-			}
-		);
-	}
+  /* Setup */
+  const tinykvm::MachineOptions options{
+      .max_mem = GUEST_MEMORY,
+      .max_cow_mem = GUEST_WORK_MEM,
+      .reset_free_work_mem = 0,
+      .vmem_base_address =
+          uint64_t(getenv("UPPER") != nullptr ? 0x40000000 : 0x0),
+      .remappings{remappings},
+      .verbose_loader = true,
+      .hugepages = (getenv("HUGE") != nullptr),
+      .relocate_fixed_mmap = (getenv("GO") == nullptr),
+      .executable_heap = dyn_elf.is_dynamic,
+  };
+  tinykvm::Machine master_vm{binary, options};
+  // master_vm.print_pagetables();
+  if (dyn_elf.is_dynamic) {
+    // TODO: figure out how to automate this
+    static const std::vector<std::string> allowed_readable_paths({
+        argv[1],
+        ".",
+        // Add all common standard libraries to the list of allowed readable
+        // paths
+        "/lib64/ld-linux-x86-64.so.2",
+        "/lib64/libgcc_s.so.1",
+        "/lib64/libc.so.6",
+        "/lib64/libm.so.6",
+        "/lib64/libpthread.so.0",
+        "/lib64/libdl.so.2",
+        "/lib64/libstdc++.so.6",
+        "/lib64/librt.so.1",
+        "/lib64/libz.so.1",
+        "/lib64/libexpat.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v2/libc.so.6",
+        "/lib64/glibc-hwcaps/x86-64-v3/libc.so.6",
+        "/lib64/glibc-hwcaps/x86-64-v4/libc.so.6",
+        "/lib64/glibc-hwcaps/x86-64-v2/libstdc++.so.6",
+        "/lib64/glibc-hwcaps/x86-64-v3/libstdc++.so.6",
+        "/lib64/glibc-hwcaps/x86-64-v4/libstdc++.so.6",
 
-	master_vm.setup_linux(
-		args,
-		{"LC_TYPE=C", "LC_ALL=C", "USER=root"});
+        // SELinux Compat
+        "/lib64/libselinux.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v2/libselinux.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v3/libselinux.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v4/libselinux.so.1",
 
-	const auto rsp = master_vm.stack_address();
+        // libcap
+        "/lib64/libcap.so.2",
+        "/lib64/glibc-hwcaps/x86-64-v2/libcap.so.2",
+        "/lib64/glibc-hwcaps/x86-64-v3/libcap.so.2",
+        "/lib64/glibc-hwcaps/x86-64-v4/libcap.so.2",
 
-	uint64_t call_addr = verify_exists(master_vm, "my_backend");
+        // libpcre
+        "/lib64/libpcre2-8.so.0",
+        "/lib64/glibc-hwcaps/x86-64-v2/libpcre2-8.so.0",
+        "/lib64/glibc-hwcaps/x86-64-v3/libpcre2-8.so.0",
+        "/lib64/glibc-hwcaps/x86-64-v4/libpcre2-8.so.0",
 
-	/* Remote debugger session */
-	if (getenv("DEBUG"))
-	{
-		auto* vm = &master_vm;
-		tinykvm::tinykvm_x86regs regs;
+        // libacl
+        "/lib64/libacl.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v2/libacl.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v3/libacl.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v4/libacl.so.1",
 
-		if (getenv("VMCALL")) {
-			master_vm.run();
-		}
-		if (getenv("FORK")) {
-			master_vm.prepare_copy_on_write();
-			vm = new tinykvm::Machine {master_vm, options};
-			vm->setup_call(regs, call_addr, rsp);
-			vm->set_registers(regs);
-		} else if (getenv("VMCALL")) {
-			master_vm.setup_call(regs, call_addr, rsp);
-			master_vm.set_registers(regs);
-		}
+        // libattr
+        "/lib64/libattr.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v2/libattr.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v3/libattr.so.1",
+        "/lib64/glibc-hwcaps/x86-64-v4/libattr.so.1",
+    });
+    master_vm.fds().set_open_readable_callback([&](std::string &path) -> bool {
+      return std::find(allowed_readable_paths.begin(),
+                       allowed_readable_paths.end(),
+                       path) != allowed_readable_paths.end();
+    });
+  }
 
-		tinykvm::RSP server {filename, *vm, 2159};
-		printf("Waiting for connection localhost:2159...\n");
-		auto client = server.accept();
-		if (client != nullptr) {
-			/* Debugging session of _start -> main() */
-			printf("Connected\n");
-			try {
-				//client->set_verbose(true);
-				while (client->process_one());
-			} catch (const tinykvm::MachineException& e) {
-				printf("EXCEPTION %s: %lu\n", e.what(), e.data());
-				vm->print_registers();
-			}
-		} else {
-			/* Resume execution normally */
-			vm->run();
-		}
-		/* Exit after debugging */
-		return 0;
-	}
+  master_vm.setup_linux(args, {"LC_TYPE=C", "LC_ALL=C", "USER=root"});
 
-	asm("" ::: "memory");
-	auto t0 = time_now();
-	asm("" ::: "memory");
+  const auto rsp = master_vm.stack_address();
 
-	/* Normal execution of _start -> main() */
-	try {
-		master_vm.run();
-	} catch (const tinykvm::MachineException& me) {
-		master_vm.print_registers();
-		fprintf(stderr, "Machine exception: %s  Data: 0x%lX\n", me.what(), me.data());
-		throw;
-	} catch (...) {
-		master_vm.print_registers();
-		throw;
-	}
+  uint64_t call_addr = verify_exists(master_vm, "my_backend");
 
-	asm("" ::: "memory");
-	auto t1 = time_now();
-	asm("" ::: "memory");
+  /* Remote debugger session */
+  if (getenv("DEBUG")) {
+    auto *vm = &master_vm;
+    tinykvm::tinykvm_x86regs regs;
 
-	if (call_addr == 0x0) {
-		double t = nanodiff(t0, t1) / 1e9;
-		printf("Time: %fs Return value: %ld\n", t, master_vm.return_value());
-		return 0;
-	}
+    if (getenv("VMCALL")) {
+      master_vm.run();
+    }
+    if (getenv("FORK")) {
+      master_vm.prepare_copy_on_write();
+      vm = new tinykvm::Machine{master_vm, options};
+      vm->setup_call(regs, call_addr, rsp);
+      vm->set_registers(regs);
+    } else if (getenv("VMCALL")) {
+      master_vm.setup_call(regs, call_addr, rsp);
+      master_vm.set_registers(regs);
+    }
 
-	/* Fork master VM */
-	master_vm.prepare_copy_on_write();
-	tinykvm::Machine vm{master_vm, options};
+    tinykvm::RSP server{filename, *vm, 2159};
+    printf("Waiting for connection localhost:2159...\n");
+    auto client = server.accept();
+    if (client != nullptr) {
+      /* Debugging session of _start -> main() */
+      printf("Connected\n");
+      try {
+        // client->set_verbose(true);
+        while (client->process_one())
+          ;
+      } catch (const tinykvm::MachineException &e) {
+        printf("EXCEPTION %s: %lu\n", e.what(), e.data());
+        vm->print_registers();
+      }
+    } else {
+      /* Resume execution normally */
+      vm->run();
+    }
+    /* Exit after debugging */
+    return 0;
+  }
 
-	/* Make a VM function call */
-	tinykvm::tinykvm_regs regs;
-	vm.setup_call(regs, call_addr, rsp);
-	//regs.rip = vm.entry_address_if_usermode();
-	vm.set_registers(regs);
-	printf("Calling fork at 0x%lX\n", call_addr);
-	vm.run(8.0f);
+  asm("" ::: "memory");
+  auto t0 = time_now();
+  asm("" ::: "memory");
 
-	/* Re-run */
-	//vm.reset_to(master_vm, options);
+  /* Normal execution of _start -> main() */
+  try {
+    master_vm.run();
+  } catch (const tinykvm::MachineException &me) {
+    master_vm.print_registers();
+    fprintf(stderr, "Machine exception: %s  Data: 0x%lX\n", me.what(),
+            me.data());
+    throw;
+  } catch (...) {
+    master_vm.print_registers();
+    throw;
+  }
 
-	vm.setup_call(regs, call_addr, rsp);
-	//regs.rip = vm.entry_address_if_usermode();
-	vm.set_registers(regs);
-	printf("Calling fork at 0x%lX\n", call_addr);
-	vm.run(8.0f);
+  asm("" ::: "memory");
+  auto t1 = time_now();
+  asm("" ::: "memory");
+
+  if (call_addr == 0x0) {
+    double t = nanodiff(t0, t1) / 1e9;
+    printf("Time: %fs Return value: %ld\n", t, master_vm.return_value());
+    return 0;
+  }
+
+  /* Fork master VM */
+  master_vm.prepare_copy_on_write();
+  tinykvm::Machine vm{master_vm, options};
+
+  /* Make a VM function call */
+  tinykvm::tinykvm_regs regs;
+  vm.setup_call(regs, call_addr, rsp);
+  // regs.rip = vm.entry_address_if_usermode();
+  vm.set_registers(regs);
+  printf("Calling fork at 0x%lX\n", call_addr);
+  vm.run(8.0f);
+
+  /* Re-run */
+  // vm.reset_to(master_vm, options);
+
+  vm.setup_call(regs, call_addr, rsp);
+  // regs.rip = vm.entry_address_if_usermode();
+  vm.set_registers(regs);
+  printf("Calling fork at 0x%lX\n", call_addr);
+  vm.run(8.0f);
 }
 
-timespec time_now()
-{
-	timespec t;
-	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &t);
-	return t;
+timespec time_now() {
+  timespec t;
+  clock_gettime(CLOCK_THREAD_CPUTIME_ID, &t);
+  return t;
 }
-long nanodiff(timespec start_time, timespec end_time)
-{
-	return (end_time.tv_sec - start_time.tv_sec) * (long)1e9 + (end_time.tv_nsec - start_time.tv_nsec);
+long nanodiff(timespec start_time, timespec end_time) {
+  return (end_time.tv_sec - start_time.tv_sec) * (long)1e9 +
+         (end_time.tv_nsec - start_time.tv_nsec);
 }
